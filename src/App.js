66875c7be46c4bdb324d845bc48ed606dd0b0771
Fredrick Oluoch <menty44@gmail.com>
2025-05-06 06:01:16 +0300

    Url interface definition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+interface ShortenedUrl {
+  shortUrl: string;
+  longUrl: string;
+}
+
 function App() {
   return (
     <div className="App">
